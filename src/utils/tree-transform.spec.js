50f3945a098654b4f2fec2114c1e45075b1618d4
caele <tsm@qlik.com>
2020-01-22 20:23:56 +0100

    chore: update test setup

--- src/utils/tree-transform.spec.js
+++ src/utils/tree-transform.spec.js
@@ -1,12 +1,12 @@
 import { createNodes } from './tree-transform';
 
-const matrix = [[{ qText: '007', qElemNumber: 0 }, { qText: '-' }]];
-
 describe('Tree transform', () => {
   describe('createNodes', () => {
-    it('should create a tree', () => {});
-    const node = createNodes(matrix, []);
-    expect(node.id).to.equal('007');
+    const matrix = [[{ qText: '007', qElemNumber: 0 }, { qText: '-' }]];
+    it('should create a tree', () => {
+      const node = createNodes(matrix, []);
+      expect(node.id).to.equal('007');
+    });
   });
   // Tests to add
   // should check for cycles and output something
