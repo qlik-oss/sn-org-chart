0f6d6ce3187d086633694552459bc521c0952678
niekvanstaveren <niek.vanstaveren@qlik.com>
2020-01-24 16:27:04 +0100

    fix: css fixes

--- src/card/card.js
+++ src/card/card.js
@@ -1,10 +1,10 @@
 export default (data, cardStyling) => {
-  let html = `<div style=${cardStyling.label}>${data.attributes.label || data.id}</div>`;
+  let html = `<div class="org-text" style=${cardStyling.label}>${data.attributes.label || data.id}</div>`;
   if (data.attributes.subLabel) {
-    html += `<div style=${cardStyling.subLabel}>${data.attributes.subLabel}</div>`;
+    html += `<div class="org-text" style=${cardStyling.subLabel}>${data.attributes.subLabel}</div>`;
   }
   if (data.attributes.details) {
-    html += `<div style=${cardStyling.details}>${data.attributes.details}</div>`;
+    html += `<div class="org-text" style=${cardStyling.details}>${data.attributes.details}</div>`;
   }
-  return `<div class="orgCard">${html}</div>`;
+  return `<div class="org-card">${html}</div>`;
 };
