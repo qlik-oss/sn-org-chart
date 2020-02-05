a286506aaa28cb161e6883d54098ba7e9fbc92a8
niekvanstaveren <niek.vanstaveren@qlik.com>
2020-02-05 12:01:54 +0100

    fix: add svg path

--- src/index.js
+++ src/index.js
@@ -39,7 +39,8 @@ export default function supernova(env) {
             setLinked(!linked);
           },
           icon: {
-            d: 'svg path',
+            d:
+              'M7.5,1 L7.5,5 L4.999,5 L4.999,7.395 L6,7.395 L6,6 L12,6 L12,10 L6,10 L6,8.395 L4.999,8.395 L5,12.4 C5,12.4414214 5.02518398,12.4769607 5.06107549,12.4921415 L5.1,12.5 L9,12.499 L9,11 L15,11 L15,15 L9,15 L9,13.5 L5.1,13.5 C4.53298766,13.5 4.06617613,13.0709893 4.00645465,12.5198571 L4,12.4 L3.999,5 L1.5,5 L1.5,1 L7.5,1 Z M14,12 L10,12 L10,14 L14,14 L14,12 Z M11,7 L7,7 L7,9 L11,9 L11,7 Z M6.5,2 L2.5,2 L2.5,4 L6.5,4 L6.5,2 Z',
           },
         }),
         [linked]
@@ -47,13 +48,13 @@ export default function supernova(env) {
 
       useEffect(() => {
         const addKeyPress = event => {
-          if (event.key === 'Shift') {
+          if (event.key === 'Shift' && selectionsAPI.isActive()) {
             setLinked(true);
           }
         };
 
         const removeKeyPress = event => {
-          if (event.key === 'Shift') {
+          if (event.key === 'Shift' && selectionsAPI.isActive()) {
             setLinked(false);
           }
         };
