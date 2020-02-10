711fcee390b35ed4768d7ebbf8539a8cd134a148
Elise Eborn <fdq@qlik.com>
2020-02-10 12:01:34 +0100

    fix: one tooltip div

--- src/tree/box.js
+++ src/tree/box.js
@@ -8,6 +8,17 @@ export default function box(divBox, o, appendNodes, cardStyling, reRender) {
     return `width:${o.nodeSize.width}px;height:${o.nodeSize.height}px; top:${o.y(p)}px;left:${o.x(p)}px;`;
   }
 
+  function getTooltipStyle(p) {
+    if (p.data.id === 'Root') {
+      return `top:${o.y(p) + 55}px;left:${o.x(p) + 140}px;visibility: visible; opacity: 1;`;
+    }
+    return `width:200px;height:50px; top:${o.y(p) - 25}px;left:${o.x(p)}px;visibility: visible;opacity: 1;`;
+  }
+
+  const tooltip = divBox
+    .append('div')
+    .attr('class', 'tooltip');
+
   divBox
     .selectAll('.node')
     .data(appendNodes)
@@ -21,5 +32,16 @@ export default function box(divBox, o, appendNodes, cardStyling, reRender) {
         reRender(node.data.id);
       }
     })
-    .html(d => card(d.data, cardStyling));
+    .html(d => card(d.data, cardStyling))
+    .on('mouseover', d => {
+      tooltip
+        .html(`${d.data.attributes.label || ''}<br />${d.data.attributes.subLabel || ''}<br />${d.data.attributes.extraLabel || ''}<br />${d.data.measure || ''}<br />`)
+        .attr('style', 'visibility: visible;opacity: 1;')
+        .attr('style', () => { getTooltipStyle(d); });
+    })
+    .on('mouseout', () => {
+      tooltip
+        .html('')
+        .attr('style', 'visibility: hidden;opacity: 0;');
+    });
 }
