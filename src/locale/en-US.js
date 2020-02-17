e2ecf0d37e0cc2d7bbf4918073aaa4c7629639a1
caele <tsm@qlik.com>
2020-02-17 15:11:40 +0100

    chore: update strings

--- src/locale/en-US.js
+++ src/locale/en-US.js
@@ -1,15 +1,16 @@
 // Local translations when used outside of sense
 export default {
   'Object.OrgChart': 'Org chart', // Name of chart, to be added to sense when native
-  'Object.OrgChart.Description': 'Org chart', // Description of chart, to be added to sense when native
-  'Object.OrgChart.MaxData': 'Max data limit reached, tree might display incorrectly',
-  'Object.OrgChart.DummyRoot': 'Dummy root',
-  'Object.OrgChart.DummyWarn': 'No single rootnode, dummy nodes inserted.',
-  'Object.OrgChart.CycleWarning': 'Data contains cycle, nodes are omitted.',
-  'Object.OrgChart.MissingRoot': 'No root node, check your data for cycles.',
+  'Object.OrgChart.Description':
+    'Displays the relations of an organization or similarly structured data. Starts at the top and allows for navigation through the hierachy.', // Description of chart, to be added to sense when native
+  'Object.OrgChart.MaxData': 'The maximum data limit is reached. The tree may display incorrectly.',
+  'Object.OrgChart.DummyRoot': 'Root root',
+  'Object.OrgChart.DummyWarn': 'No single root, root nodes inserted.',
+  'Object.OrgChart.CycleWarning': 'Data contains circular references, nodes are omitted.',
+  'Object.OrgChart.MissingRoot': 'No root node, check your data for circular references.',
   'Object.OrgChart.LabelExpression': 'Label expression',
-  'Object.OrgChart.SubLabelExpression': 'Sub label expression',
-  'Object.OrgChart.ExtraLabelExpression': 'Extra label expression',
+  'Object.OrgChart.SubLabelExpression': 'Sub-label expression',
+  'Object.OrgChart.ExtraLabelExpression': 'Extra-label expression',
   'Object.OrgChart.ColorLabelExpression': 'Color expression',
   'Object.OrgChart.BackgroundColor': 'Background color',
   'Object.OrgChart.FontColor': 'Font color',
