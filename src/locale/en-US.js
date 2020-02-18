c5d600060b3e8c53febf9c6436b9d6ff9f9afd0e
caele <tsm@qlik.com>
2020-02-18 11:24:16 +0100

    chore: remove dummy tooltip

--- src/locale/en-US.js
+++ src/locale/en-US.js
@@ -4,7 +4,6 @@ export default {
   'Object.OrgChart.Description':
     'Displays the relations of an organization or similarly structured data. Starts at the top and allows for navigation through the hierachy.', // Description of chart, to be added to sense when native
   'Object.OrgChart.MaxData': 'The maximum data limit is reached. The tree may display incorrectly.',
-  'Object.OrgChart.DummyRoot': 'Root root',
   'Object.OrgChart.DummyWarn': 'The data contains multiple root nodes.',
   'Object.OrgChart.CycleWarning': 'Data contains circular references, nodes are omitted.',
   'Object.OrgChart.MissingRoot': 'No root node, check your data for circular references.',
