34b1c6efe324a024c73b4730c2ab459d50425cc0
Max Gefvert <max.gefvert@gmail.com>
2020-05-14 13:53:30 +0200

    fix: add rgba to resolveExpression (#147)
    * fix: add rgba to resolveExpression
    
    * refactor: darkColor return
    
    * test: update unit
    
    * fix: no conversion for rgba
    
    * fix: correct regex

--- src/utils/color-utils.js
+++ src/utils/color-utils.js
@@ -2,16 +2,21 @@
 import CSSColors from './css-colors';
 
 const colorUtils = {
-  resolveExpression: input => {
+  resolveExpression: (input) => {
     // rgb
     let matches = /^rgb\(\s*(\d{1,3})\s*,\s*(\d{1,3})\s*,\s*(\d{1,3})\s*\)$/i.exec(input);
     if (matches) {
       return `rgb(${matches[1]},${matches[2]},${matches[3]})`;
     }
+    // rgba
+    matches = /^rgba\(\s*(\d{1,3})\s*,\s*(\d{1,3})\s*,\s*(\d{1,3})\s*,\s*(\d(\.\d+)?)\s*\)$/i.exec(input);
+    if (matches) {
+      return `rgba(${matches[1]},${matches[2]},${matches[3]},${matches[4]})`;
+    }
     // argb
     matches = /^argb\(\s*(\d{1,3})\s*,\s*(\d{1,3})\s*,\s*(\d{1,3})\s*,\s*(\d{1,3})\s*\)$/i.exec(input);
     if (matches) {
-      const a = Math.round((matches[1] / 255) * 100) / 100;
+      const a = Math.round(matches[1] / 2.55) / 100;
       return `rgba(${matches[2]},${matches[3]},${matches[4]},${a})`;
     }
     // hex
@@ -40,8 +45,9 @@ const colorUtils = {
       R = rgba ? parseInt(f[0].slice(5), 10) : parseInt(f[0].slice(4), 10);
       G = parseInt(f[1], 10);
       B = parseInt(f[2], 10);
-      return `${(rgba ? 'rgba(' : 'rgb(') + (Math.round((0 - R) * percent) + R)},${Math.round((0 - G) * percent) +
-        G},${Math.round((0 - B) * percent) + B}${rgba ? `,${f[3]}` : ')'}`;
+      return `${(rgba ? 'rgba(' : 'rgb(') + (Math.round((0 - R) * percent) + R)},${Math.round((0 - G) * percent) + G},${
+        Math.round((0 - B) * percent) + B
+      }${rgba ? `,${f[3]}` : ')'}`;
     }
     f = parseInt(color.slice(1), 16);
     R = f >> 16;
