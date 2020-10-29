ac36dbe8ea076abf88b1f363114b1100be43c277
Niek van Staveren <Niek.vanstaveren@qlik.com>
2020-10-29 16:51:50 +0100

    fix: title encoding (#183)

--- src/card/card.js
+++ src/card/card.js
@@ -1,4 +1,5 @@
 import colorUtils from '../utils/color-utils';
+import encodeUtils from '../utils/encoder';
 import constants from '../tree/size-constants';
 
 export function getBackgroundColor(data, cardStyling) {
@@ -23,15 +24,15 @@ export default (data, cardStyling, selectionObj) => {
   const backgroundColor = getBackgroundColor(data, cardStyling);
   const fontColor = getFontColor(cardStyling, backgroundColor);
   const attributes = data.attributes || {};
-  let html = `<div class="sn-org-card-title">${attributes.label || data.id}</div>`;
+  let html = `<div class="sn-org-card-title">${encodeUtils.encodeTitle(attributes.label || data.id)}</div>`;
   if (attributes.subLabel) {
-    html += `<div class="sn-org-card-label">${attributes.subLabel}</div>`;
+    html += `<div class="sn-org-card-label">${encodeUtils.encodeTitle(attributes.subLabel)}</div>`;
   }
   if (data.measure) {
     const measureLabel = cardStyling.measureLabel ? `${cardStyling.measureLabel}: ` : '';
-    html += `<div class="sn-org-card-label">${measureLabel}${data.measure}</div>`;
+    html += `<div class="sn-org-card-label">${encodeUtils.encodeTitle(measureLabel)}${data.measure}</div>`;
   } else if (attributes.extraLabel) {
-    html += `<div class="sn-org-card-label">${attributes.extraLabel}</div>`;
+    html += `<div class="sn-org-card-label">${encodeUtils.encodeTitle(attributes.extraLabel)}</div>`;
   }
   const selectedClass = api && api.isActive() ? (isSelected ? ' selected' : ' not-selected') : '';
 
