28772d44d55784261b15925f62f0868c25089b71
Elise Eborn <fdq@qlik.com>
2021-11-25 16:22:44 +0100

    chore: add api spec

--- src/object-properties.js
+++ src/object-properties.js
@@ -7,18 +7,22 @@ const properties = {
     qSuppressMissing: true,
   },
   /**
+   * Show title for the visualization
    * @type {boolean}
    */
   showTitles: true,
   /**
+   * Visualization title
    * @type {string}
    */
   title: '',
   /**
+   * Visualization subtitle
    * @type {string}
    */
   subtitle: '',
   /**
+   * Visualization footnote
    * @type {string}
    */
   footnote: '',
@@ -27,77 +31,117 @@ const properties = {
    */
   navigationMode: 'free',
   /**
+   * Resize and pan chart when a node's list of children is expanded
    * @type {boolean}
    */
   resizeOnExpand: false,
   /**
+   * Holds chart styling
    * @type {object}
    */
   style: {
     /**
+     * Holds font color details
      * @type {object}
      */
     fontColor: {
       /**
+       * How the font color is input
        * @type {'auto'|'colorPicker'|'byExpression'}
        */
       colorType: 'auto',
       /**
+       * Font color
        * @type {object}
        */
       color: {
+        /**
+         * Index of theme color
+         * @type {number}
+         */
         index: -1,
+        /**
+         *  Color defined by rgb, argb or html
+         * @type {string}
+         */
         color: '#484848',
       },
       /**
+       * An expression to decide font color
        * @type {string}
        */
       colorExpression: '',
     },
     /**
+     * Hold background color details
      * @type {object}
      */
     backgroundColor: {
       /**
+       * How the background color is input
        * @type {'auto'|'colorPicker'|'byExpression'}
        */
       colorType: 'colorPicker',
       /**
+       * Background color
        * @type {object}
        */
       color: {
+        /**
+         * Index of theme color
+         * @type {number}
+         */
         index: -1,
+        /**
+        *  Color defined by rgb, argb or html
+        * @type {string}
+        */
         color: '#ffffff',
       },
       /**
+       * An expression to decide background color
        * @type {string}
        */
       colorExpression: '',
     },
     /**
+     * Holds border details
      * @type {object}
      */
     border: {
       /**
+       * Shop top border
        * @type {boolean}
        */
       top: true,
       /**
+       * Show full border
        * @type {boolean}
        */
       fullBorder: true,
       /**
-       * @type {string}
+       * How the border color is input
+       * @type {'auto'|'colorPicker'|'byExpression'}
        */
       colorType: 'auto',
       /**
+       * Border color
        * @type {object}
        */
       color: {
+        /**
+         * Index of theme color
+         * @type {number}
+         */
         index: -1,
+        /**
+         *  Color defined by rgb, argb or html
+         * @type {string}
+         */
         color: '#737373',
       },
       /**
+       * An expression to decide border color
        * @type {string}
        */
       colorExpression: '',
