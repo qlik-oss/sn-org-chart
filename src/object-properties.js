f1712fcd80701ccb21f68bf3abf181efb713b68b
Elise Eborn <fdq@qlik.com>
2021-12-01 08:26:47 +0100

    docs: add type definitions and generate api spec

--- src/object-properties.js
+++ src/object-properties.js
@@ -37,9 +37,54 @@ const properties = {
   resizeOnExpand: false,
   /**
    * Holds chart styling
-   * @type {object}
+   * @type {Style}
    */
   style: {},
 };
+
+/**
+ * Holds styling options
+ * @typedef {object} Style
+ * @property {FontColor=} fontColor - Color of the text, by default #484848
+ * @property {Background=} backgroundColor - Color of the background, by default #e6e6e6
+ * @property {Border=} border - Styling for border, by default #737373
+ */
+
+/**
+ * @typedef {object} FontColor
+ * @property {('auto'|'colorPicker'|'byExpression')=} colorType - How the font color is defined, defaults to auto
+ * @property {PaletteColor=} color - Color defined by index or hex code, needed if colorType is colorPicker
+ * @property {ColorExpression=} colorExpression - Color defined by expression, needed if colorType is byExpression
+ */
+
+/**
+ * @typedef {object} Background
+ * @property {('auto'|'colorPicker'|'byExpression')=} colorType - How the font color is defined, defaults to colorPicker
+ * @property {PaletteColor=} color - Color defined by index or hex code, needed if colorType is colorPicker
+ * @property {ColorExpression=} colorExpression - Color defined by expression, needed if colorType is byExpression
+ */
+
+/**
+ * @typedef {object} Border
+ * @property {boolean=} top - Set to true to show thicker top border, default is true
+ * @property {boolean=} fullBorder - Set to true to show full border, default is true
+  * @property {('auto'|'colorPicker'|'byExpression')=} colorType - How the border color is defined, defaults to auto
+ * @property {PaletteColor=} color - Color defined by index or hex code, needed if colorType is colorPicker
+ * @property {ColorExpression=} colorExpression - Color defined by expression, needed if colorType is byExpression
+ */
+
+/**
+ * Color information structure. Holds the actual color and index in palette.
+ * @typedef {object} PaletteColor
+ * @property {string=} color - Color as hex string (only used if index: -1)
+ * @property {number=} index - Index in palette
+ */
+
+/**
+ * Format for using color expressions
+ * @typedef {object} ColorExpression
+ * @property {object} qStringExpression
+ * @property {string} qStringExpression.qExpr - expression that resolves to a supported color format
+ */
 
 export default properties;
