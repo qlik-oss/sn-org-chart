54781b8659f189b93314383d6fbafa83796359c2
Elise <fdq@qlik.com>
2022-01-28 14:37:49 +0100

    docs: add api spec (#241)
    * chore: add api spec
    
    * docs: add type definitions and generate api spec
    
    * fix: add defaults in different way
    
    * fix: scriptappy conf
    
    * fix: set correct defaults
    
    * chore: added back scriptappy dep
    
    * fix: comment

--- src/object-properties.js
+++ src/object-properties.js
@@ -1,3 +1,8 @@
+/**
+ * @extends {qae.GenericObjectProperties}
+ * @entry
+ */
+
 import DEFAULTS from './style-defaults';
 
 const properties = {
@@ -9,31 +14,38 @@ const properties = {
     qSuppressMissing: true,
   },
   /**
+   * Show title for the visualization
    * @type {boolean}
    */
   showTitles: true,
   /**
+   * Visualization title
    * @type {string}
    */
   title: '',
   /**
+   * Visualization subtitle
    * @type {string}
    */
   subtitle: '',
   /**
+   * Visualization footnote
    * @type {string}
    */
   footnote: '',
   /**
-   * @type {'regular'|'scroll'|'free'}
+   * How the org chart is navigated
+   * @type {'expandAll'|'free'}
    */
   navigationMode: 'free',
   /**
+   * Resize and pan chart when a node's list of children is expanded
    * @type {boolean}
    */
   resizeOnExpand: false,
   /**
-   * @type {object}
+   * Holds chart styling
+   * @type {Style}
    */
   style: {
     fontColor: {
@@ -55,5 +67,50 @@ const properties = {
     },
   },
 };
+
+/**
+ * Holds styling options
+ * @typedef {object} Style
+ * @property {FontColor=} fontColor - Color of the text, by default #484848
+ * @property {Background=} backgroundColor - Color of the background, by default #ffffff
+ * @property {Border=} border - Styling for border, by default #737373
+ */
+
+/**
+ * @typedef {object} FontColor
+ * @property {('auto'|'colorPicker'|'byExpression')=} [colorType='auto'] How the font color is defined
+ * @property {PaletteColor=} color - Color defined by index or hex code, needed if colorType is colorPicker
+ * @property {ColorExpression=} colorExpression - Color defined by expression, needed if colorType is byExpression
+ */
+
+/**
+ * @typedef {object} Background
+ * @property {('auto'|'colorPicker'|'byExpression')=} [colorType='colorPicker'] How the background color is defined
+ * @property {PaletteColor=} color - Color defined by index or hex code, needed if colorType is colorPicker
+ * @property {ColorExpression=} colorExpression - Color defined by expression, needed if colorType is byExpression
+ */
+
+/**
+ * @typedef {object} Border
+ * @property {boolean=} [top=true] Set to true to show thicker top border
+ * @property {boolean=} [fullBorder=true] Set to true to show full border
+ * @property {('auto'|'colorPicker'|'byExpression')=} [colorType='auto'] How the border color is defined
+ * @property {PaletteColor=} color - Color defined by index or hex code, needed if colorType is colorPicker
+ * @property {ColorExpression=} colorExpression - Color defined by expression, needed if colorType is byExpression
+ */
+
+/**
+ * Color information structure. Holds the actual color and index in palette.
+ * @typedef {object} PaletteColor
+ * @property {string=} color - Color as hex string (only used if index: -1)
+ * @property {number=} index - Index in palette
+ */
+
+/**
+ * Format for using color expressions
+ * @typedef {object} ColorExpression
+ * @property {object} qStringExpression
+ * @property {string} qStringExpression.qExpr - expression that resolves to a supported color format
+ */
 
 export default properties;
