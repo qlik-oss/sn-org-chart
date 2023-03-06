abd5f38c0f320910865bea938ec8131317eed6e0
Niek van Staveren <niek.vanstaveren@qlik.com>
2023-03-06 20:51:28 +0100

    chore: set unit test threshold

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,10 @@
 module.exports = {
   testEnvironment: 'jsdom',
   testRegex: 'src/.+\\.(test|spec)\\.[jt]sx?$',
+  coverageThreshold: {
+    global: {
+      lines: 76,
+    },
+  },
+  coveragePathIgnorePatterns: ['src/__tests__/default-orgchart-props.js'],
 };
