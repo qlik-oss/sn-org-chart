2e42ba643f4dfda7e15bbaa4b671b4cf54c71350
Niek van Staveren <Niek.vanstaveren@qlik.com>
2023-03-07 12:52:16 +0100

    chore: set unit test threshold (#283)
    * chore: set unit test threshold
    
    * chore: update threshold
    
    * chore: only collect coverage src folder
    
    * chore: proper coverage

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,11 @@
 module.exports = {
   testEnvironment: 'jsdom',
   testRegex: 'src/.+\\.(test|spec)\\.[jt]sx?$',
+  coverageThreshold: {
+    global: {
+      lines: 64,
+    },
+  },
+  coveragePathIgnorePatterns: ['src/__tests__/default-orgchart-props.js', 'src/styles', 'src/extension', 'src/data.js'],
+  collectCoverageFrom: ['src/**'],
 };
