a0a8f5f8a0d2249c9bedeefe13e7a27216a90034
Niek van Staveren <niek.vanstaveren@qlik.com>
2023-04-14 12:48:11 +0200

    test: use chromium browser

--- test/rendering/playwright.config.js
+++ test/rendering/playwright.config.js
@@ -1,7 +1,15 @@
+import { devices } from '@playwright/test';
+
 const config = {
   testDir: './',
   outputDir: 'test-results',
   testMatch: /.*\.spec\.js/,
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+  ],
 };
 
 export default config;
