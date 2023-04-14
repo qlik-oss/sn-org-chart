770c1de9a8086d6e0505e92dd730a6da7194c466
Niek van Staveren <niek.vanstaveren@qlik.com>
2023-04-14 12:53:31 +0200

    Revert "test: use chromium browser"
    This reverts commit a0a8f5f8a0d2249c9bedeefe13e7a27216a90034.

--- test/rendering/playwright.config.js
+++ test/rendering/playwright.config.js
@@ -1,15 +1,7 @@
-import { devices } from '@playwright/test';
-
 const config = {
   testDir: './',
   outputDir: 'test-results',
   testMatch: /.*\.spec\.js/,
-  projects: [
-    {
-      name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
-    },
-  ],
 };
 
 export default config;
