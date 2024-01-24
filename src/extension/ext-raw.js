38e0e4481e21c06dbab70a48663efcdc793fbcd0
Ana Yakushi <bqg@qlik.com>
2024-01-24 10:22:08 +0100

    presentation properties behind flag

--- src/extension/ext-raw.js
+++ src/extension/ext-raw.js
@@ -148,129 +148,137 @@ const getSettings = (translator, flags, anything) => {
               },
             },
           },
-          /*
-          backgroundColor: {
-            type: "items",
-            items: {
-              useColorExpression: {
-                ref: "style.backgroundColor.colorType",
-                type: "string",
-                translation: "Object.OrgChart.BackgroundColor",
-                component: "dropdown",
-                defaultValue: DEFAULTS.BACKGROUND_COLOR_TYPE,
-                options: colorOptions,
-              },
-              colorPicker: {
-                component: "color-picker",
-                type: "object",
-                ref: "style.backgroundColor.color",
-                translation: "properties.color",
-                dualOutput: true,
-                defaultValue: DEFAULTS.BACKGROUND_COLOR,
-                show: (data) =>
-                  propertyResolver.getValue(data, "style.backgroundColor.colorType") === "colorPicker",
-              },
-              colorExpression: {
-                component: "string",
-                type: "string",
-                ref: "style.backgroundColor.colorExpression",
-                translation: "Common.Expression",
-                expression: "optional",
-                defaultValue: "",
-                show: (data) =>
-                  propertyResolver.getValue(data, "style.backgroundColor.colorType") === "byExpression",
-              },
-            },
-          },
-          fontColor: {
-            type: "items",
-            items: {
-              useColorExpression: {
-                ref: "style.fontColor.colorType",
-                type: "string",
-                translation: "Object.OrgChart.FontColor",
-                component: "dropdown",
-                defaultValue: DEFAULTS.FONT_COLOR_TYPE,
-                options: colorOptions,
-              },
-              colorPicker: {
-                component: "color-picker",
-                type: "object",
-                ref: "style.fontColor.color",
-                translation: "properties.color",
-                dualOutput: true,
-                defaultValue: DEFAULTS.FONT_COLOR_DARK,
-                show: (data) => propertyResolver.getValue(data, "style.fontColor.colorType") === "colorPicker",
-              },
-              colorExpression: {
-                component: "string",
-                type: "string",
-                ref: "style.fontColor.colorExpression",
-                translation: "Common.Expression",
-                expression: "optional",
-                defaultValue: "",
-                show: (data) => propertyResolver.getValue(data, "style.fontColor.colorType") === "byExpression",
-              },
-            },
-          },
-          border: {
-            type: "items",
-            items: {
-              appearanceHeader: {
-                component: "text",
-                translation: "Object.OrgChart.CardAppearance",
-                style: "pp-nm-hcd__list-header",
-              },
-              topBar: {
-                type: "boolean",
-                ref: "style.border.top",
-                translation: "Object.OrgChart.TopBar",
-                defaultValue: DEFAULTS.BORDER_TOP,
-              },
-              fullBorder: {
-                type: "boolean",
-                ref: "style.border.fullBorder",
-                translation: "properties.border",
-                defaultValue: DEFAULTS.BORDER_FULL,
-              },
-              colorType: {
-                component: "dropdown",
-                type: "string",
-                ref: "style.border.colorType",
-                translation: "properties.border.color",
-                defaultValue: DEFAULTS.BORDER_COLOR_TYPE,
-                options: colorOptions,
-                show: (data) => bordersActive(data),
-              },
-              colorPicker: {
-                component: "color-picker",
-                type: "object",
-                ref: "style.border.color",
-                translation: "properties.color",
-                dualOutput: true,
-                defaultValue: DEFAULTS.BORDER_COLOR,
-                show: (data) =>
-                  bordersActive(data) &&
-                  propertyResolver.getValue(data, "style.border.colorType") === "colorPicker",
-              },
-              colorExpression: {
-                component: "string",
-                type: "string",
-                ref: "style.border.colorExpression",
-                translation: "Common.Expression",
-                expression: "optional",
-                defaultValue: "",
-                show: (data) =>
-                  bordersActive(data) &&
-                  propertyResolver.getValue(data, "style.border.colorType") === "byExpression",
-              },
-            },
-          },
-          */
+          backgroundColor: {},
+          fontColor: {},
+          border: {},
         },
       },
     },
   };
+
+  if (!flags?.isEnabled('SENSECLIENT_IM_5036_VIZBUNDLE_STYLING')) {
+
+    settings.items.styling.items.backgroundColor = {
+      type: "items",
+      items: {
+        useColorExpression: {
+          ref: "style.backgroundColor.colorType",
+          type: "string",
+          translation: "Object.OrgChart.BackgroundColor",
+          component: "dropdown",
+          defaultValue: DEFAULTS.BACKGROUND_COLOR_TYPE,
+          options: colorOptions,
+        },
+        colorPicker: {
+          component: "color-picker",
+          type: "object",
+          ref: "style.backgroundColor.color",
+          translation: "properties.color",
+          dualOutput: true,
+          defaultValue: DEFAULTS.BACKGROUND_COLOR,
+          show: (data) =>
+            propertyResolver.getValue(data, "style.backgroundColor.colorType") === "colorPicker",
+        },
+        colorExpression: {
+          component: "string",
+          type: "string",
+          ref: "style.backgroundColor.colorExpression",
+          translation: "Common.Expression",
+          expression: "optional",
+          defaultValue: "",
+          show: (data) =>
+            propertyResolver.getValue(data, "style.backgroundColor.colorType") === "byExpression",
+        },
+      },
+    };
+
+    settings.items.styling.items.fontColor = {
+      type: "items",
+      items: {
+        useColorExpression: {
+          ref: "style.fontColor.colorType",
+          type: "string",
+          translation: "Object.OrgChart.FontColor",
+          component: "dropdown",
+          defaultValue: DEFAULTS.FONT_COLOR_TYPE,
+          options: colorOptions,
+        },
+        colorPicker: {
+          component: "color-picker",
+          type: "object",
+          ref: "style.fontColor.color",
+          translation: "properties.color",
+          dualOutput: true,
+          defaultValue: DEFAULTS.FONT_COLOR_DARK,
+          show: (data) => propertyResolver.getValue(data, "style.fontColor.colorType") === "colorPicker",
+        },
+        colorExpression: {
+          component: "string",
+          type: "string",
+          ref: "style.fontColor.colorExpression",
+          translation: "Common.Expression",
+          expression: "optional",
+          defaultValue: "",
+          show: (data) => propertyResolver.getValue(data, "style.fontColor.colorType") === "byExpression",
+        },
+      },
+    };
+
+    settings.items.styling.items.border = {
+      type: "items",
+      items: {
+        appearanceHeader: {
+          component: "text",
+          translation: "Object.OrgChart.CardAppearance",
+          style: "pp-nm-hcd__list-header",
+        },
+        topBar: {
+          type: "boolean",
+          ref: "style.border.top",
+          translation: "Object.OrgChart.TopBar",
+          defaultValue: DEFAULTS.BORDER_TOP,
+        },
+        fullBorder: {
+          type: "boolean",
+          ref: "style.border.fullBorder",
+          translation: "properties.border",
+          defaultValue: DEFAULTS.BORDER_FULL,
+        },
+        colorType: {
+          component: "dropdown",
+          type: "string",
+          ref: "style.border.colorType",
+          translation: "properties.border.color",
+          defaultValue: DEFAULTS.BORDER_COLOR_TYPE,
+          options: colorOptions,
+          show: (data) => bordersActive(data),
+        },
+        colorPicker: {
+          component: "color-picker",
+          type: "object",
+          ref: "style.border.color",
+          translation: "properties.color",
+          dualOutput: true,
+          defaultValue: DEFAULTS.BORDER_COLOR,
+          show: (data) =>
+            bordersActive(data) &&
+            propertyResolver.getValue(data, "style.border.colorType") === "colorPicker",
+        },
+        colorExpression: {
+          component: "string",
+          type: "string",
+          ref: "style.border.colorExpression",
+          translation: "Common.Expression",
+          expression: "optional",
+          defaultValue: "",
+          show: (data) =>
+            bordersActive(data) &&
+            propertyResolver.getValue(data, "style.border.colorType") === "byExpression",
+        },
+      },
+    };
+  }
+
   return settings;
 };  
 
