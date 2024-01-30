cbfda165713b2dab08b0cced6ff29233c4fffc4e
Ana Yakushi <bqg@qlik.com>
2024-01-30 16:49:03 +0100

    add components to styling panel

--- src/extension/styling-definitions/styling-definition.js
+++ src/extension/styling-definitions/styling-definition.js
@@ -22,8 +22,10 @@ function createStylingDefinition(theme, flags, translator) {
     },
   });
 
-  const bordersActive = (data) =>
-    propertyResolver.getValue(data, 'style.border.top') || propertyResolver.getValue(data, 'style.border.fullBorder');
+  const bordersActive = (data) => {
+    return (propertyResolver.getValue(data, 'card.border.top') ?? DEFAULTS.BORDER_TOP) ||
+      (propertyResolver.getValue(data, 'card.border.fullBorder') ?? DEFAULTS.BORDER_FULL);
+  };
 
   return {
     component: 'styling-panel',
@@ -61,166 +63,172 @@ function createStylingDefinition(theme, flags, translator) {
           },
         },
       },
-      /*
-      cardSection: {
-        translation: 'properties.card.label',
-        component: 'panel-section',
-        items: {
-          contrastColor: {
-            component: 'checkbox',
-            type: 'boolean',
-            ref: 'label.value.useContrastColor',
-            translation: 'properties.dataPoints.valuelabel.contrast',
-            defaultValue: true,
-          },
-        },
-      },
-      */
-      fontColor: {
+      fontColorSection: {
         component: 'panel-section',
         type: 'items',
         translation: 'Object.OrgChart.FontColor',
         items: {
-          fontColorWrapperItem: {
-            component: 'inline-wrapper',
+          fontColorItems: {
+            component: 'items',
+            ref: 'components',
+            key: 'label',
             items: {
-              useColorExpression: {
-                ref: 'style.fontColor.colorType',
-                width: 9,
-                //type: 'string',
-                //translation: 'Object.OrgChart.FontColor',
-                component: 'dropdown',
-                defaultValue: DEFAULTS.FONT_COLOR_TYPE,
-                options: colorOptions,
-                //placeholder: 'properties.colorMode.primary',
+              fontColorWrapperItem: {
+                component: 'inline-wrapper',
+                items: {
+                  useColorExpression: {
+                    ref: 'label.value.fontColor.colorType',
+                    width: 9,
+                    //type: 'string',
+                    //translation: 'Object.OrgChart.FontColor',
+                    component: 'dropdown',
+                    defaultValue: DEFAULTS.FONT_COLOR_TYPE,
+                    options: colorOptions,
+                    //placeholder: 'properties.colorMode.primary',
+                  },
+                  colorPicker: {
+                    component: 'color-picker',
+                    //type: 'object',
+                    ref: 'label.value.fontColor.color',
+                    width: 3,
+                    //translation: 'properties.color',
+                    dualOutput: true,
+                    defaultValue: DEFAULTS.FONT_COLOR_DARK,
+                    show: (data) => (propertyResolver.getValue(data, 'label.value.fontColor.colorType') ?? DEFAULTS.FONT_COLOR_TYPE) === 'colorPicker',
+                  },
+                },
               },
-              colorPicker: {
-                component: 'color-picker',
-                //type: 'object',
-                ref: 'style.fontColor.color',
-                width: 3,
-                //translation: 'properties.color',
-                dualOutput: true,
-                defaultValue: DEFAULTS.FONT_COLOR_DARK,
-                show: (data) => propertyResolver.getValue(data, 'style.fontColor.colorType') === 'colorPicker',
+              colorExpression: {
+                component: 'input-field-expression',
+                type: 'string',
+                ref: 'label.value.fontColor.colorExpression',
+                //translation: 'Common.Expression',
+                expression: 'optional',
+                defaultValue: '',
+                show: (data) => (propertyResolver.getValue(data, 'label.value.fontColor.colorType') ?? DEFAULTS.FONT_COLOR_TYPE) === 'byExpression',
               },
             },
           },
-          colorExpression: {
-            component: 'input-field-expression',
-            type: 'string',
-            ref: 'style.fontColor.colorExpression',
-            //translation: 'Common.Expression',
-            expression: 'optional',
-            defaultValue: '',
-            show: (data) => propertyResolver.getValue(data, 'style.fontColor.colorType') === 'byExpression',
-          },
         },
       },
-      backgroundColor: {
+      backgroundColorSection: {
         component: 'panel-section',
         //type: 'items',
         translation: 'Object.OrgChart.ColorLabelExpression',
         items: {
-          fontColorWrapperItem: {
-            component: 'inline-wrapper',
+          backgroundColorItems: {
+            component: 'items',
+            ref: 'components',
+            key: 'card',
             items: {
-              useColorExpression: {
-                ref: 'style.backgroundColor.colorType',
-                width: 9,
-                //type: 'string',
-                //translation: 'Object.OrgChart.BackgroundColor',
-                component: 'dropdown',
-                defaultValue: DEFAULTS.BACKGROUND_COLOR_TYPE,
-                options: colorOptions,
+              fontColorWrapperItem: {
+                component: 'inline-wrapper',
+                items: {
+                  useColorExpression: {
+                    ref: 'card.backgroundColor.colorType',
+                    width: 9,
+                    //type: 'string',
+                    //translation: 'Object.OrgChart.BackgroundColor',
+                    component: 'dropdown',
+                    defaultValue: DEFAULTS.BACKGROUND_COLOR_TYPE,
+                    options: colorOptions,
+                  },
+                  colorPicker: {
+                    component: 'color-picker',
+                    type: 'object',
+                    ref: 'card.backgroundColor.color',
+                    width: 3,
+                    //translation: 'properties.color',
+                    dualOutput: true,
+                    defaultValue: DEFAULTS.BACKGROUND_COLOR,
+                    show: (data) =>
+                      (propertyResolver.getValue(data, 'card.backgroundColor.colorType') ?? DEFAULTS.BACKGROUND_COLOR_TYPE) === 'colorPicker',
+                  },
+                },
               },
-              colorPicker: {
-                component: 'color-picker',
-                type: 'object',
-                ref: 'style.backgroundColor.color',
-                width: 3,
-                //translation: 'properties.color',
-                dualOutput: true,
-                defaultValue: DEFAULTS.BACKGROUND_COLOR,
+              colorExpression: {
+                component: 'input-field-expression',
+                //type: 'string',
+                ref: 'card.backgroundColor.colorExpression',
+                //translation: 'Common.Expression',
+                expression: 'optional',
+                defaultValue: '',
                 show: (data) =>
-                  propertyResolver.getValue(data, 'style.backgroundColor.colorType') === 'colorPicker',
+                  (propertyResolver.getValue(data, 'card.backgroundColor.colorType') ?? DEFAULTS.BACKGROUND_COLOR_TYPE) === 'byExpression',
               },
             },
-          },
-          colorExpression: {
-            component: 'input-field-expression',
-            //type: 'string',
-            ref: 'style.backgroundColor.colorExpression',
-            //translation: 'Common.Expression',
-            expression: 'optional',
-            defaultValue: '',
-            show: (data) =>
-              propertyResolver.getValue(data, 'style.backgroundColor.colorType') === 'byExpression',
           },
         },
       },
-      border: {
+      cardBorderSection: {
         component: 'panel-section',
         translation: 'Object.OrgChart.CardAppearance',
         items: {
-          /*
-          appearanceHeader: {
-            component: 'text',
-            translation: 'Object.OrgChart.CardAppearance',
-            style: 'pp-nm-hcd__list-header',
-          },
-          */
-          topBar: {
-            //type: 'boolean',
-            component: 'checkbox',
-            ref: 'style.border.top',
-            translation: 'Object.OrgChart.TopBar',
-            defaultValue: DEFAULTS.BORDER_TOP,
-          },
-          fullBorder: {
-            //type: 'boolean',
-            component: 'checkbox',
-            ref: 'style.border.fullBorder',
-            translation: 'properties.border',
-            defaultValue: DEFAULTS.BORDER_FULL,
-          },
-          fontColorWrapperItem: {
-            component: 'inline-wrapper',
+          cardBorderItems: {
+            component: 'items',
+            ref: 'components',
+            key: 'card',
             items: {
-              colorType: {
-                component: 'dropdown',
-                type: 'string',
-                ref: 'style.border.colorType',
-                width: 9,
-                translation: 'properties.border.color',
-                defaultValue: DEFAULTS.BORDER_COLOR_TYPE,
-                options: colorOptions,
-                show: (data) => bordersActive(data),
+              /*
+              appearanceHeader: {
+                component: 'text',
+                translation: 'Object.OrgChart.CardAppearance',
+                style: 'pp-nm-hcd__list-header',
               },
-              colorPicker: {
-                component: 'color-picker',
-                type: 'object',
-                ref: 'style.border.color',
-                width: 3,
-                translation: 'properties.color',
-                dualOutput: true,
-                defaultValue: DEFAULTS.BORDER_COLOR,
+              */
+              topBar: {
+                //type: 'boolean',
+                component: 'checkbox',
+                ref: 'card.border.top',
+                translation: 'Object.OrgChart.TopBar',
+                defaultValue: DEFAULTS.BORDER_TOP,
+              },
+              fullBorder: {
+                //type: 'boolean',
+                component: 'checkbox',
+                ref: 'card.border.fullBorder',
+                translation: 'properties.border',
+                defaultValue: DEFAULTS.BORDER_FULL,
+              },
+              fontColorWrapperItem: {
+                component: 'inline-wrapper',
+                items: {
+                  colorType: {
+                    component: 'dropdown',
+                    type: 'string',
+                    ref: 'card.border.colorType',
+                    width: 9,
+                    translation: 'properties.border.color',
+                    defaultValue: DEFAULTS.BORDER_COLOR_TYPE,
+                    options: colorOptions,
+                    show: (data) => bordersActive(data),
+                  },
+                  colorPicker: {
+                    component: 'color-picker',
+                    type: 'object',
+                    ref: 'style.border.color',
+                    width: 3,
+                    translation: 'properties.color',
+                    dualOutput: true,
+                    defaultValue: DEFAULTS.BORDER_COLOR,
+                    show: (data) =>
+                      bordersActive(data) &&
+                      (propertyResolver.getValue(data, 'card.border.colorType') ?? DEFAULTS.BORDER_COLOR_TYPE) === 'colorPicker',
+                  },
+                },
+              },
+              colorExpression: {
+                component: 'input-field-expression',
+                type: 'string',
+                ref: 'style.border.colorExpression',
+                translation: 'Common.Expression',
+                expression: 'optional',
+                defaultValue: '',
                 show: (data) =>
                   bordersActive(data) &&
-                  propertyResolver.getValue(data, 'style.border.colorType') === 'colorPicker',
-              },
+                  (propertyResolver.getValue(data, 'style.border.colorType') ?? DEFAULTS.BORDER_COLOR_TYPE) === 'byExpression',
+             },
             },
-          },
-          colorExpression: {
-            component: 'input-field-expression',
-            type: 'string',
-            ref: 'style.border.colorExpression',
-            translation: 'Common.Expression',
-            expression: 'optional',
-            defaultValue: '',
-            show: (data) =>
-              bordersActive(data) &&
-              propertyResolver.getValue(data, 'style.border.colorType') === 'byExpression',
           },
         },
       },
