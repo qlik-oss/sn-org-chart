331cc9fe61ee4169b77c43e16f2d4dd4bb4a868f
Niek van Staveren <Niek.vanstaveren@qlik.com>
2024-03-15 09:35:09 +0100

    test: fix test report folder (#351)

--- playwright.config.js
+++ playwright.config.js
@@ -9,6 +9,16 @@ const config = {
   use: {
     baseURL: "http://localhost:8077",
   },
+  reporter: [
+    ["list"],
+    [
+      "html",
+      {
+        outputFolder: "./test/rendering/test-report",
+        open: process.env.CI ? "never" : "on-failure",
+      },
+    ],
+  ],
 };
 
 export default config;
