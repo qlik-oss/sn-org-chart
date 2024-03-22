170b071df2cfa9441802ecb09c0a7a9120650555
Ana Yakushi <bqg@qlik.com>
2024-03-22 09:36:14 +0100

    test missing components array

--- src/extension/__tests__/migration.spec.js
+++ src/extension/__tests__/migration.spec.js
@@ -27,13 +27,45 @@ describe("migration", () => {
       };
     });
 
+    it("should create the components array when it does not exist and migrate the properties from style", () => {
+      const result = migrateStyle(properties);
+      expect(result.components).toContainEqual({
+        key: "label",
+        label: {
+          value: {
+            colorType: "fontColorType",
+            color: "fontColor",
+            colorExpression: "fontColorExpression",
+          },
+        },
+      });
+      expect(result.components).toContainEqual({
+        key: "backgroundColor",
+        backgroundColor: {
+          colorType: "backgroundColorType",
+          color: "backgroundColor",
+          colorExpression: "backgroundColorExpression",
+        },
+      });
+      expect(result.components).toContainEqual({
+        key: "border",
+        border: {
+          top: "top",
+          fullBorder: "fullBorder",
+          colorType: "bordercolorType",
+          color: "borderColor",
+          colorExpression: "borderColorExpression",
+        },
+      });
+    });
+
     it("should not migrate the properties when components has elements already", () => {
       properties.components = [{ key: "label" }, { key: "backgroundColor" }, { key: "border" }];
       const result = migrateStyle(properties);
       expect(result.components).toEqual([{ key: "label" }, { key: "backgroundColor" }, { key: "border" }]);
     });
 
-    it("should migrate the properties when components is empty", () => {
+    it("should migrate the properties when components exists but is empty", () => {
       properties.components = [];
       const result = migrateStyle(properties);
       const labelComponent = {
